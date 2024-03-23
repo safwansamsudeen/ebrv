ad979bad5aae958f4ba920d1b1741af80dd8a205
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-03-23 23:34:54 +0530

    feat: allow mass-despatch of DCs

--- ebrv/public/js/doctype_list.js
+++ ebrv/public/js/doctype_list.js
@@ -1,12 +1,40 @@
 frappe.listview_settings["Delivery Challan"] = {
 	refresh: function (listview) {
 		// add button in action
-		console.log();
-		listview.page.add_action_item("Add to Despatch", (selected) => {
-			const id = frappe.prompt("Despatch ID?");
-			listview.get_checked_items().map(async ({ name }) => {
-				await frappe.db.set_value("Delivery Challan", name, "despatch", id);
-			});
+
+		listview.page.add_action_item("Add to Despatch", () => {
+			const checkedItems = listview.get_checked_items();
+			if (!checkedItems.length) {
+				frappe.throw("Please select atleast one Delivery Challan.");
+			}
+			frappe.prompt(
+				{
+					label: "Despatch ID",
+					fieldname: "despatch_id",
+					fieldtype: "Link",
+					options: "Despatch",
+				},
+				({ despatch_id }) => {
+					checkedItems.map(async ({ name }) => {
+						try {
+							await frappe.db.set_value(
+								"Delivery Challan",
+								name,
+								"despatch",
+								despatch_id
+							);
+						} catch (e) {
+							console.log(e);
+							frappe.throw("There was an error while setting the Despatches.");
+						}
+					});
+					frappe.msgprint(
+						`Successfully set <b>${checkedItems.length}</b> DC${
+							checkedItems.length !== 1 ? "s" : ""
+						} to despatch <b>${despatch_id}</b>`
+					);
+				}
+			);
 		});
 	},
 };
