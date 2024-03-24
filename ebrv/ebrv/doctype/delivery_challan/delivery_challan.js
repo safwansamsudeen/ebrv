c4edea19ba15d41832b1e2bedf336f51eac68026
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-03-24 17:40:17 +0530

    feat: screen 5

--- ebrv/ebrv/doctype/delivery_challan/delivery_challan.js
+++ ebrv/ebrv/doctype/delivery_challan/delivery_challan.js
@@ -9,7 +9,26 @@ function update_total_copies(doc) {
 	);
 }
 
+function uuidv4() {
+	return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+		(+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+	);
+}
+
 frappe.ui.form.on("Delivery Challan", {
+	print_order(frm) {
+		frm.set_query("delivery_location", function () {
+			return {
+				filters: [["print_order", "=", frm.doc.print_order]],
+			};
+		});
+	},
+	before_save: (frm) => {
+		if (frm.doc.workflow_state1 === "Despatched") {
+			console.log("in");
+			frappe.model.set_value("Delivery Challan", frm.doc.name, "ebrv_id", uuidv4());
+		}
+	},
 	number_of_bundles(frm) {
 		update_total_copies(frm.doc);
 	},
